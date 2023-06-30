4b44bf2704516ad1d1834adff877d46de3f3f86d
Vitaliy Kochubey <Slesar_v@mail.ru>
2023-06-30 09:30:24 +0700

    test

--- src/main/java/searchengine/Application.java
+++ src/main/java/searchengine/Application.java
@@ -1,7 +1,6 @@
 package searchengine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class Application{
